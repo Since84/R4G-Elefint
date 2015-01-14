55f9ad531139c2824563a83e7ae33b0f6a7f6173
Damon Hastings <damon@elefintdesigns.com>
2015-01-13 17:36:50 -0800

    Style and Home Page setup

---
+++ home.php
@@ -0,0 +1,21 @@
+<?php
+	get_header();
+
+	//Get Timber Context. Provides Data to TWIG views
+	$context = Timber::get_context();
+
+	//Get Site Header : uses SparkHeader Class
+	$sparkHeader = new Header_Header(array(
+	  'showLogo'      =>  true
+	  ,'headerRight'  =>  'sidebars/sidebar-social.php' //Template for right side ( /views/components/... )
+	  ,'nav'          =>  'main-nav' //Menu name for nav menu
+	  ,'template'     =>  'header' //Name of header template
+	  ,'isJs'         =>  false
+	));	
+	// $context['header'] = $sparkHeader::getView();
+
+	//Display Page using home template 
+	// Timber::render('/views/pages/home.html.twig', $context);
+
+	get_footer();
+?>
